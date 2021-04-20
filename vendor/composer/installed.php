f20bc1228513379f03a6835849e71a5bf8d70ee9
Davi Cruz <daviondejesus@gmail.com>
2021-04-20 13:51:22 -0300

    init: Autoload
    Iniciando o projeto com autoload de arquivos

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'b32480f18788fd8d5c4b2583c26797b1b6f80acf',
+    'name' => 'davicruzof/desafio',
+  ),
+  'versions' => 
+  array (
+    'davicruzof/desafio' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b32480f18788fd8d5c4b2583c26797b1b6f80acf',
+    ),
+  ),
+);
